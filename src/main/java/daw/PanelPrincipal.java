532164550b78fd9d0e2980fbe9e8b8a71a85c537
arid0910 <arid0910@g.educaand.es>
2024-04-22 09:05:33 +0200

    0 - 9 con texto

--- src/main/java/daw/PanelPrincipal.java
+++ src/main/java/daw/PanelPrincipal.java
@@ -15,7 +15,7 @@ import javax.swing.JTextArea;
 /**
  * @author adam
  */
-public class PanelPrincipal extends JPanel {
+public class PanelPrincipal extends JPanel implements ActionListener {
 
     // Atributos de la clase (privados)
     private PanelBotones botonera;
@@ -34,14 +34,31 @@ public class PanelPrincipal extends JPanel {
         botonera = new PanelBotones();
         // Creamos el área de texto
         areaTexto = new JTextArea(10, 50);
-        areaTexto.setEditable(false);
+        areaTexto.setEditable(true);
         areaTexto.setBackground(Color.white);
-
         //Establecemos layout del panel principal
         this.setLayout(new BorderLayout());
         // Colocamos la botonera y el área texto
         this.add(areaTexto, BorderLayout.NORTH);
         this.add(botonera, BorderLayout.SOUTH);
+        actionBtn();
 
     }
+
+    public final void actionBtn() {
+        for (int i = 0; i < 15; i++) {
+            botonera.grupoBotones[i].addActionListener(this);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == botonera.grupoBotones[i]) {
+                areaTexto.setText(Integer.toString(i));
+                break;
+            }
+        }
+    }
+
 }
